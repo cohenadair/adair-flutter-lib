e5ebfc327701e3cef95570816ca1ceb65f7258c3
Cohen Adair <cohenadair@gmail.com>
2025-12-27 13:31:38 -0500

    Update capturePrintStatements to return a Future

--- test/test_utils/async.dart
+++ test/test_utils/async.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
 
 /// Returns a list of all logs printed to the console.
-List<String> capturePrintStatements(void Function() work) {
+Future<List<String>> capturePrintStatements(
+  FutureOr<void> Function() work,
+) async {
   final result = <String>[];
 
   // Run the code in a custom Zone that intercepts print.
-  runZoned(
+  await runZoned<FutureOr<void>>(
     work,
     zoneSpecification: ZoneSpecification(
       print: (_, __, ___, line) => result.add(line),
