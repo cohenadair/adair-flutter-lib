17f8ed0aa641d760e0a7e119474941b66d0667bf
Cohen Adair <cohenadair@gmail.com>
2026-03-12 20:30:53 -0400

    Add Stream support to SafeFutureBuilder; rename to AsyncBuilder (#3)

--- test/widgets/async_builder_test.dart
+++ test/widgets/async_builder_test.dart
@@ -1,4 +1,6 @@
-import 'package:adair_flutter_lib/widgets/safe_future_builder.dart';
+import 'dart:async';
+
+import 'package:adair_flutter_lib/widgets/async_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -8,14 +10,14 @@ void main() {
   testWidgets("Loading widget shows while future is working", (tester) async {
     await pumpContext(
       tester,
-      (_) => SafeFutureBuilder<bool>(
+      (_) => AsyncBuilder<bool>.future(
         future: Future.delayed(
           const Duration(milliseconds: 50),
           () => Future.value(true),
         ),
         errorReason: "Error",
         loadingBuilder: (_) => Text("Loading..."),
-        builder: (_, __) => Text("Test Text"),
+        builder: (_, _) => Text("Test Text"),
       ),
     );
     expect(find.text("Loading..."), findsOneWidget);
@@ -28,13 +30,13 @@ void main() {
   testWidgets("SidedBox shows while future is working", (tester) async {
     await pumpContext(
       tester,
-      (_) => SafeFutureBuilder<bool>(
+      (_) => AsyncBuilder<bool>.future(
         future: Future.delayed(
           const Duration(milliseconds: 50),
           () => Future.value(true),
         ),
         errorReason: "Error",
-        builder: (_, __) => Text("Test Text"),
+        builder: (_, _) => Text("Test Text"),
       ),
     );
     expect(find.byType(SizedBox), findsOneWidget);
@@ -47,13 +49,13 @@ void main() {
   testWidgets("Error widget is rendered", (tester) async {
     await pumpContext(
       tester,
-      (_) => SafeFutureBuilder<bool>(
+      (_) => AsyncBuilder<bool>.future(
         future: Future.delayed(
           const Duration(milliseconds: 50),
           () => throw Exception("Test exception"),
         ),
         errorReason: "Testing error",
-        builder: (_, __) => Text("Test Text"),
+        builder: (_, _) => Text("Test Text"),
         loadingBuilder: (_) => Text("Loading..."),
         errorBuilder: (_) => Text("Test error"),
       ),
@@ -70,13 +72,13 @@ void main() {
   testWidgets("Loading widget is rendered on error", (tester) async {
     await pumpContext(
       tester,
-      (_) => SafeFutureBuilder<bool>(
+      (_) => AsyncBuilder<bool>.future(
         future: Future.delayed(
           const Duration(milliseconds: 50),
           () => throw Exception("Test exception"),
         ),
         errorReason: "Testing error",
-        builder: (_, __) => Text("Test Text"),
+        builder: (_, _) => Text("Test Text"),
         loadingBuilder: (_) => Text("Loading..."),
       ),
     );
@@ -91,13 +93,13 @@ void main() {
   testWidgets("SizedBox is rendered on error", (tester) async {
     await pumpContext(
       tester,
-      (_) => SafeFutureBuilder<bool>(
+      (_) => AsyncBuilder<bool>.future(
         future: Future.delayed(
           const Duration(milliseconds: 50),
           () => throw Exception("Test exception"),
         ),
         errorReason: "Testing error",
-        builder: (_, __) => Text("Test Text"),
+        builder: (_, _) => Text("Test Text"),
       ),
     );
     // Finish future.
@@ -107,7 +109,51 @@ void main() {
     expect(find.text("Test Text"), findsNothing);
   });
 
-  testWidgets("Successful widget is rendered", (tester) async {
-    // TODO
+  testWidgets("Successful Future widget is rendered", (tester) async {
+    await pumpContext(
+      tester,
+      (_) => AsyncBuilder<bool>.future(
+        future: Future.value(true),
+        errorReason: "Error",
+        builder: (_, data) => Text("$data"),
+      ),
+    );
+    await tester.pumpAndSettle();
+    expect(find.text("true"), findsOneWidget);
+  });
+
+  testWidgets("Stream is loading", (tester) async {
+    final controller = StreamController<bool>();
+    addTearDown(controller.close);
+
+    await pumpContext(
+      tester,
+      (_) => AsyncBuilder<bool>.stream(
+        stream: controller.stream,
+        errorReason: "Error",
+        loadingBuilder: (_) => Text("Loading..."),
+        builder: (_, _) => Text("Test Text"),
+      ),
+    );
+    expect(find.text("Loading..."), findsOneWidget);
+    expect(find.text("Test Text"), findsNothing);
+
+    controller.add(true);
+    await tester.pumpAndSettle();
+    expect(find.text("Loading..."), findsNothing);
+    expect(find.text("Test Text"), findsOneWidget);
+  });
+
+  testWidgets("Successful Stream widget is rendered", (tester) async {
+    await pumpContext(
+      tester,
+      (_) => AsyncBuilder<bool>.stream(
+        stream: Stream.value(true),
+        errorReason: "Error",
+        builder: (_, data) => Text("$data"),
+      ),
+    );
+    await tester.pumpAndSettle();
+    expect(find.text("true"), findsOneWidget);
   });
 }
