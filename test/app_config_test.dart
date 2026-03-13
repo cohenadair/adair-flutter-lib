2993e6fa0af792600f66f7f453ff5b091a7bd866
Cohen Adair <cohenadair@gmail.com>
2026-03-12 20:33:03 -0400

    Add AppConfig and Io tests

--- test/app_config_test.dart
+++ test/app_config_test.dart
@@ -1,3 +1,81 @@
+import 'package:adair_flutter_lib/app_config.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
-  // TODO
+  setUp(() {
+    AppConfig.reset();
+  });
+
+  test("appName is set", () {
+    AppConfig.get.init(appName: () => "Test App");
+    expect(AppConfig.get.appName(), "Test App");
+  });
+
+  test("companyName is null when not provided", () {
+    AppConfig.get.init(appName: () => "Test App");
+    expect(AppConfig.get.companyName, isNull);
+  });
+
+  test("companyName is set when provided", () {
+    AppConfig.get.init(
+      appName: () => "Test App",
+      companyName: () => "Test Corp",
+    );
+    expect(AppConfig.get.companyName!(), "Test Corp");
+  });
+
+  test("appIcon defaults to Icons.not_interested when not provided", () {
+    AppConfig.get.init(appName: () => "Test App");
+    expect(AppConfig.get.appIcon, Icons.not_interested);
+  });
+
+  test("appIcon is set when provided", () {
+    AppConfig.get.init(appName: () => "Test App", appIcon: Icons.home);
+    expect(AppConfig.get.appIcon, Icons.home);
+  });
+
+  test("colorAppTheme defaults to Colors.pink when not provided", () {
+    AppConfig.get.init(appName: () => "Test App");
+    // ignore: deprecated_member_use
+    expect(AppConfig.get.colorAppTheme, Colors.pink);
+  });
+
+  test("colorAppTheme is set when provided", () {
+    AppConfig.get.init(
+      appName: () => "Test App",
+      // ignore: deprecated_member_use
+      colorAppTheme: Colors.blue,
+    );
+    // ignore: deprecated_member_use
+    expect(AppConfig.get.colorAppTheme, Colors.blue);
+  });
+
+  test("colorAppBarContent defaults to white when not provided", () {
+    AppConfig.get.init(appName: () => "Test App");
+    expect(AppConfig.get.colorAppBarContent(true), Colors.white);
+    expect(AppConfig.get.colorAppBarContent(false), Colors.white);
+  });
+
+  test("colorAppBarContent is set when provided", () {
+    AppConfig.get.init(
+      appName: () => "Test App",
+      colorAppBarContent: (isDark) => isDark ? Colors.black : Colors.white,
+    );
+    expect(AppConfig.get.colorAppBarContent(true), Colors.black);
+    expect(AppConfig.get.colorAppBarContent(false), Colors.white);
+  });
+
+  test("themeMode defaults to ThemeMode.system when not provided", () {
+    AppConfig.get.init(appName: () => "Test App");
+    expect(AppConfig.get.themeMode(), ThemeMode.system);
+  });
+
+  test("themeMode is set when provided", () {
+    AppConfig.get.init(
+      appName: () => "Test App",
+      themeMode: () => ThemeMode.dark,
+    );
+    expect(AppConfig.get.themeMode(), ThemeMode.dark);
+  });
 }
--- test/utils/io_test.dart
+++ test/utils/io_test.dart
@@ -1,3 +1,36 @@
+import 'dart:io';
+
+import 'package:adair_flutter_lib/utils/io.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../test_utils/stubbed_managers.dart';
+
 void main() {
-  // TODO
+  late StubbedManagers managers;
+
+  setUp(() async {
+    managers = await StubbedManagers.create();
+  });
+
+  test("isConnected returns true when example.com resolves", () async {
+    when(
+      managers.ioWrapper.lookup("example.com"),
+    ).thenAnswer((_) async => [InternetAddress("93.184.216.34")]);
+    expect(await isConnected(), isTrue);
+  });
+
+  test("isConnected returns true when only google.com resolves", () async {
+    when(managers.ioWrapper.lookup("example.com")).thenAnswer((_) async => []);
+    when(
+      managers.ioWrapper.lookup("google.com"),
+    ).thenAnswer((_) async => [InternetAddress("142.250.80.46")]);
+    expect(await isConnected(), isTrue);
+  });
+
+  test("isConnected returns false when both lookups return empty", () async {
+    when(managers.ioWrapper.lookup("example.com")).thenAnswer((_) async => []);
+    when(managers.ioWrapper.lookup("google.com")).thenAnswer((_) async => []);
+    expect(await isConnected(), isFalse);
+  });
 }
